807d4adbcc029501d330a2888989f7010e715f4c
Jimmy Reyes <jrfullstack@gmail.com>
2023-10-08 11:46:54 +0200

    alertas y funciones en el login

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,13 +1,71 @@
-// import React from 'react'
+import { useState } from "react";
+import { Global } from "../../helpers/Global";
+import { useForm } from "../../hooks/useForm";
 
 export const Login = () => {
-  return (
-      <>
-          <header className="content__header">
-              <h1 className="content__title">Login</h1>
-          </header>
+    const { form, changed } = useForm({});
+    const [saved, setSaved] = useState("not_sended");
 
-          <div className="content__posts"></div>
-      </>
-  );
-}
+    const loginUser = async (e) => {
+        e.preventDefault();
+
+        let userToLogin = form;
+
+        const request = await fetch(Global.url + "user/login", {
+            method: "POST",
+            body: JSON.stringify(userToLogin),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        // respuesta del servidor
+        const data = await request.json();
+
+        // guardar en localStorage para tener el token a mano
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+
+        // para mostrar alerta
+        if (data.status == "success") {
+            setSaved("saved");
+        } else {
+            setSaved("error");
+        }
+    };
+
+    return (
+        <>
+            <header className="content__header">
+                <h1 className="content__title">Login</h1>
+            </header>
+
+            <div className="content__posts">
+                {/* condicion */}
+                {saved == "saved" ? (<strong className="alert alert-success">Usuario identificado correctamente</strong>) : ("")}
+                {saved == "error" ? (<strong className="alert alert-danger">Usuario o contraseña incorrecta</strong>) : ("")}
+                <form onSubmit={loginUser} className="form-login">
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            onChange={changed}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            onChange={changed}
+                        />
+                    </div>
+                    <input type="submit" value="Identificate" className="" />
+                </form>
+            </div>
+        </>
+    );
+};
