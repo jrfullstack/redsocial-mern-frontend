cd9e3ac5076374fcab1207ef2045f6b136a9c5f8
Jimmy Reyes <jrfullstack@gmail.com>
2023-11-12 16:44:05 +0100

    publicaciones completado

--- src/components/layout/private/Sidebar.jsx
+++ src/components/layout/private/Sidebar.jsx
@@ -1,130 +1,205 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
+
 import useAuth from "../../../hooks/useAuth";
+import {useForm} from '../../../hooks/useForm';
+
 import { Global } from "../../../helpers/Global";
 import avatar from "../../../assets/img/user.png";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Sidebar = () => {
 
     const {auth, counters} = useAuth();
-    // console.log({auth, counters})
+    const {form, changed} = useForm();
+    const [stored, setStored] = useState("not_stored");
+
+    const savePublication =async (e) => { 
+        e.preventDefault();
+
+        const token = localStorage.getItem("token");
+
+        // recoger los datos del formulario
+        let newPublication = form;
+        newPublication.user = auth._id;
+
+        // hacer request para guardar en la db
+        const request = await fetch(Global.url + "publication/save", {
+          method: "POST",
+          body: JSON.stringify(newPublication),
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": token,
+          },
+        });
+
+        const data = await request.json();
+
+        // mostrar mensaje de exito o de error
+        if (data.status == "success") {
+            setStored("stored");
+        }else{
+            setStored("error")
+        }
+
+        // subir imagen
+        const fileInput = document.querySelector("#file");
+        if (data.status == "success" && fileInput.files[0]) {
+            const formData = new FormData();
+            formData.append("file0", fileInput.files[0]);
+            const uploadRequest = await fetch(Global.url + "publication/upload/" + data.publicationStored._id, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    // "Content-Type": "application/json",
+                    "Authorization": token,
+                },
+            });
+
+            const uploadData = await uploadRequest.json();
+
+            if (uploadData.status == "success") {
+                setStored("stored");
+            }else{
+                setStored("error");
+
+            }
+
+            if (data.status == "success" && uploadData.status == "success") {
+                
+                const myForm = document.querySelector("#publication-form");
+                myForm.reset();
+            }
+
+        }
+
+     }
 
     return (
-        <>
-            <aside className="layout__aside">
-                <header className="aside__header">
-                    {/* <h1 className="aside__title">Hola, {Auth.name}</h1> */}
-                    <h1 className="aside__title">Hola, {auth.name}</h1>
-                </header>
+      <>
+        <aside className="layout__aside">
+          <header className="aside__header">
+            {/* <h1 className="aside__title">Hola, {Auth.name}</h1> */}
+            <h1 className="aside__title">Hola, {auth.name}</h1>
+          </header>
 
-                <div className="aside__container">
-                    <div className="aside__profile-info">
-                        <div className="profile-info__general-info">
-                            <div className="general-info__container-avatar">
-                                {/* si el usuario tiene foto subida */}
-                                {auth.image != "default.png" && (
-                                    <img
-                                        src={
-                                            Global.url +
-                                            "user/avatar/" +
-                                            auth.image
-                                        }
-                                        className="container-avatar__img"
-                                        alt="Foto de perfil"
-                                    />
-                                )}
+          <div className="aside__container">
+            <div className="aside__profile-info">
+              <div className="profile-info__general-info">
+                <div className="general-info__container-avatar">
+                  {/* si el usuario tiene foto subida */}
+                  {auth.image != "default.png" && (
+                    <img
+                      src={Global.url + "user/avatar/" + auth.image}
+                      className="container-avatar__img"
+                      alt="Foto de perfil"
+                    />
+                  )}
 
-                                {/* si no tiene foto */}
-                                {auth.image == "default.png" && (
-                                    <img
-                                        src={avatar}
-                                        className="container-avatar__img"
-                                        alt="Foto de perfil"
-                                    />
-                                )}
-                            </div>
-
-                            <div className="general-info__container-names">
-                                <a href="#" className="container-names__name">
-                                    {auth.name} {auth.surname}
-                                </a>
-                                <p className="container-names__nickname">
-                                    {auth.nick}
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="profile-info__stats">
-                            <div className="stats__following">
-                                <Link to={"siguiendo/" + auth._id} className="following__link">
-                                    <span className="following__title">
-                                        Siguiendo
-                                    </span>
-                                    <span className="following__number">
-                                        {counters.following}
-                                    </span>
-                                </Link>
-                            </div>
-                            <div className="stats__following">
-                                <Link to={"siguidores/" + auth._id} className="following__link">
-                                    <span className="following__title">
-                                        Seguidores
-                                    </span>
-                                    <span className="following__number">
-                                        {counters.followed}
-                                    </span>
-                                </Link>
-                            </div>
-
-                            <div className="stats__following">
-                                <a href="#" className="following__link">
-                                    <span className="following__title">
-                                        Publicaciones
-                                    </span>
-                                    <span className="following__number">
-                                        {counters.publications}
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="aside__container-form">
-                        <form className="container-form__form-post">
-                            <div className="form-post__inputs">
-                                <label
-                                    htmlFor="post"
-                                    className="form-post__label">
-                                    ¿Que estas pesando hoy?
-                                </label>
-                                <textarea
-                                    name="post"
-                                    className="form-post__textarea"></textarea>
-                            </div>
-
-                            <div className="form-post__inputs">
-                                <label
-                                    htmlFor="image"
-                                    className="form-post__label">
-                                    Sube tu foto
-                                </label>
-                                <input
-                                    type="file"
-                                    name="image"
-                                    className="form-post__image"
-                                />
-                            </div>
-
-                            <input
-                                type="submit"
-                                value="Enviar"
-                                className="form-post__btn-submit"
-                                disabled
-                            />
-                        </form>
-                    </div>
+                  {/* si no tiene foto */}
+                  {auth.image == "default.png" && (
+                    <img
+                      src={avatar}
+                      className="container-avatar__img"
+                      alt="Foto de perfil"
+                    />
+                  )}
                 </div>
-            </aside>
-        </>
+
+                <div className="general-info__container-names">
+                  <a href="#" className="container-names__name">
+                    {auth.name} {auth.surname}
+                  </a>
+                  <p className="container-names__nickname">{auth.nick}</p>
+                </div>
+              </div>
+
+              <div className="profile-info__stats">
+                <div className="stats__following">
+                  <Link
+                    to={"siguiendo/" + auth._id}
+                    className="following__link">
+                    <span className="following__title">Siguiendo</span>
+                    <span className="following__number">
+                      {counters.following}
+                    </span>
+                  </Link>
+                </div>
+                <div className="stats__following">
+                  <Link
+                    to={"siguidores/" + auth._id}
+                    className="following__link">
+                    <span className="following__title">Seguidores</span>
+                    <span className="following__number">
+                      {counters.followed}
+                    </span>
+                  </Link>
+                </div>
+
+                <div className="stats__following">
+                  <a href="#" className="following__link">
+                    <span className="following__title">Publicaciones</span>
+                    <span className="following__number">
+                      {counters.publications}
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="aside__container-form">
+              {/* condicion */}
+              {stored == "stored" ? (
+                <strong className="alert alert-success">
+                  Publicada correctamente!!
+                </strong>
+              ) : (
+                ""
+              )}
+              {stored == "error" ? (
+                <strong className="alert alert-danger">
+                  No se pudo guardar la publicacion
+                </strong>
+              ) : (
+                ""
+              )}
+              <form
+                id="publication-form"
+                className="container-form__form-post"
+                onSubmit={savePublication}
+              >
+                <div className="form-post__inputs">
+                  <label htmlFor="text" className="form-post__label">
+                    ¿Que estas pesando hoy?
+                  </label>
+                  <textarea
+                    name="text"
+                    className="form-post__textarea"
+                    onChange={changed}
+                  />
+                </div>
+
+                <div className="form-post__inputs">
+                  <label htmlFor="file" className="form-post__label">
+                    Sube tu foto
+                  </label>
+                  <input
+                    type="file"
+                    name="file0"
+                    id="file"
+                    className="form-post__image"
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value="Enviar"
+                  className="form-post__btn-submit"
+                />
+              </form>
+            </div>
+          </div>
+        </aside>
+      </>
     );
 }
